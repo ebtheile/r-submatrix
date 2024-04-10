08ab801948a949c2366309af75dbbf98a085138c
Jakub <koubele.jakub@gmail.com>
2024-04-10 12:41:09 +0200

    Bugfixes.

--- jakub_solution.r
+++ jakub_solution.r
@@ -4,16 +4,13 @@ max_5_5_submatrix <- function(m) {
   kernel = matrix(1, 5, 5) 
   conv = conv2(m, kernel, shape = "valid")
   max_index <- which.max(conv)
-  row_index <- (max_index - 1) %/% nrow(conv) + 1
-  col_index <- (max_index - 1) %% nrow(conv) + 1
-  result = m[(row_index):(row_index+5), (col_index):(col_index+5)]
+  col_index <- (max_index - 1) %/% nrow(conv) + 1
+  row_index <- (max_index - 1) %% nrow(conv) + 1
+  result = m[(row_index):(row_index+4), (col_index):(col_index+4)]
   return (result)
 }
 
 m <- matrix(rnorm(10000),nrow=100)
+print(system.time(result <-max_5_5_submatrix(m)))
 
-start.time <- Sys.time()
-result = max_5_5_submatrix(m)
-end.time <- Sys.time()
 
-print(end.time - start.time)
